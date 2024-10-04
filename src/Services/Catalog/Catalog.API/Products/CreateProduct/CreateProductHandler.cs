d94c78c1bcb26042b316824ff4d658c69bfe4322
markgamal32 <96923190+markgamal32@users.noreply.github.com>
2024-10-04 16:51:37 +0300

    develop commandHandeler to save product to DB using Martin Library

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,35 +1,32 @@
-﻿using BuildingBlocks.CQRS;
-using Catalog.API.Models;
-using Marten;
-using MediatR;
+﻿namespace Catalog.API.Products.CreateProduct;
 
-namespace Catalog.API.Products.CreateProduct
+
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+:ICommand<CreateProductResult>;
+public record CreateProductResult(Guid Id);
+
+
+internal class CreateProductCommandHandler (IDocumentSession session)
+	: ICommandHandler<CreateProductCommand, CreateProductResult>
 {
-
-	public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
-	:ICommand<CreateProductResult>;
-	public record CreateProductResult(Guid Id);
-
-
-	internal class CreateProductCommandHandler (IDocumentSession session)
-		: ICommandHandler<CreateProductCommand, CreateProductResult>
+	public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
 	{
-		public Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
-		{
-			// create product entity from command object
-			Product product = new Product {
-				Name = command.Name,
-				Category = command.Category,
-				Description = command.Description,
-				ImageFile = command.ImageFile,
-				Price = command.Price
-			};
-			
+		// create product entity from command object
+		Product product = new Product {
+			Name = command.Name,
+			Category = command.Category,
+			Description = command.Description,
+			ImageFile = command.ImageFile,
+			Price = command.Price
+		};
+		
 
-			// save in database 
-			// return result 
-			// business logic to create product
-			throw new NotImplementedException();
-		}
+		// save in database 
+		session.Store(product);
+		await session.SaveChangesAsync(cancellationToken);
+
+
+		// return result 
+		return new CreateProductResult(product.Id);
 	}
 }
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,10 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add Services to the Container.
-
+builder.Services.AddCarter();
+builder.Services.AddMediatR(
+	config => { config.RegisterServicesFromAssembly(typeof(Program).Assembly); }
+	);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.MapCarter();
 
 app.Run();
